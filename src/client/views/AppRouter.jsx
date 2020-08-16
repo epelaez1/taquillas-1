4558610cd91f70c045a286c0bb7b078fdbe8a2df
Enrique Pelaez <e.pelaez@alumnos.upm.es>
2020-08-16 09:11:51 +0200

    Design the Lockers view and add it to the AppRouter

--- src/client/views/AppRouter.jsx
+++ src/client/views/AppRouter.jsx
@@ -10,6 +10,7 @@ import ErrorView from './ErrorView';
 import SignUp from './SignUp/SignUp';
 import Catalog from './Catalog/Catalog';
 import Profile from './Profile/Profile';
+import Lockers from './Lockers';
 import EndSession from '../components/EndSession';
 import Layout from '../components/Layout/Layout';
 import LogOut from './LogOut';
@@ -21,7 +22,7 @@ const AppRouter = () => {
 		<Fragment key="adminRoutes">
 			<Route exact path="/admin/locations" component={TestView} />
 			<Route exact path="/admin/payments" component={TestView} />
-			<Route exact path="/admin/lockers" component={TestView} />
+			<Route exact path="/admin/lockers" component={Lockers} />
 			<Route exact path="/admin/users" component={TestView} />
 			<Route exact path="/admin/rentals" component={TestView} />
 			<Route exact path="/admin/requests" component={TestView} />
